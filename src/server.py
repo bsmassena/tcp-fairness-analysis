9e03b233765541529b9ed074c6eefba826cf7f4e
Bruno <bsmlima@outlook.com>
2019-12-13 07:06:30 -0300

    Shows Bps every second

--- src/server.py
+++ src/server.py
@@ -1,13 +1,28 @@
 import socket
+import csv
+import time
 from threading import Thread
+import numpy as np
+import matplotlib.pyplot as plt
 
 def process_connection(sock, addr):
     bytes_received = 0
+    start = time.time()
+    ys = []
+
     while True:
         data = sock.recv(4096)
         bytes_received += len(data)
         if not data: break
-        print(bytes_received)
+        if time.time() - start <= 1: continue
+
+        ys.append(bytes_received/1000)
+        # plt.plot(ys)
+        # plt.pause(0.0001)
+
+        print(bytes_received*8)
+        start = time.time()
+        bytes_received = 0
 
 
 HOST = ('', 4000)
